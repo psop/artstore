6bc00c9e759e7c606c57d8559a0cb77b108bfd96
LizHuang <search.psop@gmail.com.tw>
2017-08-15 00:13:50 +0800

    uploader

--- lib/carrierwave/storage/fog.rb
+++ lib/carrierwave/storage/fog.rb
@@ -6,6 +6,7 @@ CarrierWave.configure do |config|
     aws_secret_access_key: ENV['carrier_wave_aws_secret_access_key'],                        # required
     region:                'us-east-1',                  # optional, defaults to 'us-east-1'
   }
+  config.storage = :fog
   config.fog_directory  = 'travelfuntaiwan'                          # required
   config.fog_public     = false                                        # optional, defaults to true
   config.fog_attributes = { cache_control: "public, max-age=#{365.day.to_i}" } # optional, defaults to {}
